e2f597ce492a14ef0ad046a5f85119e63636bd2b
glen newton <glen.newton@gmail.com>
2015-04-08 08:11:20 -0400

    Taxonomic units load working

--- itis.go
+++ itis.go
@@ -1,32 +1,165 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"reflect"
+	"strconv"
+	"time"
+)
+
+type IsTable interface {
+	Name() string
+	KeyFieldName() string
+	Prepare(interface{})
+	Clear() bool
+}
+
+type IsTable2 struct {
+}
+
+func (it *IsTable2) KeyFieldName() string {
+	return "Tsn"
+}
+
+func (it *IsTable2) Name() string {
+	return "taxonomic_units"
+}
+
+func (it *IsTable2) Prepare(interface{}) {
+	// if .Update_date_tmp != nil {
+	// 	tmp := string(t.Update_date_tmp)
+	// 	t.Update_date = &tmp
+	// }
+}
+
+func (t *TaxonomicUnit) Instance() interface{} {
+	return new(TaxonomicUnit)
+}
+
+func (t *TaxonomicUnit) KeyFieldName() string {
+	return "Tsn"
+}
+
+func (t *TaxonomicUnit) Clear() bool {
+	t.Tsn = 0
+	t.Complete_name = nil
+	t.Completeness_rtng = nil
+	t.Credibility_rtng = nil
+	t.Currency_rating = nil
+	t.Hybrid_author_id = 0
+	t.Initial_time_stamp = nil
+	t.Kingdom_id = 0
+	t.N_usage = nil
+	t.Name_usage = nil
+	t.Parent_tsn = nil
+	t.Phylo_sort_seq = 0
+	t.Rank_id = 0
+	t.Taxon_author_id = 0
+	t.Unaccept_reason = nil
+	t.Uncertain_prnt_ind = nil
+	t.Unit_ind1 = nil
+	t.Unit_ind2 = nil
+	t.Unit_ind3 = nil
+	t.Unit_ind4 = nil
+	t.Unit_name1 = nil
+	t.Unit_name2 = nil
+	t.Unit_name3 = nil
+	t.Unit_name4 = nil
+	t.Unnamed_taxon_ind = nil
+	t.Update_date = nil
+	tmp := new([]uint8)
+	t.Update_date_tmp = *tmp
+	t.Update_string = nil
+	return false
+}
+
+func (t *TaxonomicUnit) Name() string {
+	return "taxonomic_units"
+}
+
+func (t *TaxonomicUnit) Prepare(interface{}) {
+	if t.Update_date_tmp != nil {
+		tmp := string(t.Update_date_tmp)
+		t.Update_date = &tmp
+	}
+}
+
 type TaxonomicUnit struct {
-	Tsn                int64      `db:"tsn"`
-	Unit_ind1          *string    `db:"unit_ind1"`
-	Unit_name1         *string    `dbtsn,"unit_name1"`
-	Unit_ind2          *string    `db:"unit_ind2"`
-	Unit_name2         *string    `db:"unit_name2"`
-	Unit_ind3          *string    `db:"unit_ind3"`
-	Unit_name3         *string    `db:"unit_name3"`
-	Unit_ind4          *string    `db:"unit_ind4"`
-	Unit_name4         *string    `db:"unit_name4"`
-	Unnamed_taxon_ind  *string    `db:"unnamed_taxon_ind"`
-	Name_usage         *string    `db:"name_usage"`
-	Unaccept_reason    *string    `db:"unaccept_reason"`
-	Credibility_rtng   *string    `db:"credibility_rtng"`
-	Completeness_rtng  *string    `db:"completeness_rtng"`
-	Currency_rating    *string    `db:"currency_rating"`
-	Phylo_sort_seq     int32      `db:"phylo_sort_seq"`
-	Initial_time_stamp *time.Time `db:"initial_time_stamp"`
-	Parent_tsn         *int64     `db:"parent_tsn"`
-	Taxon_author_id    int64      `db:"taxon_author_id"`
-	Hybrid_author_id   int64      `db:"hybrid_author_id"`
-	Kingdom_id         uint8      `db:"kingdom_id"`
-	Rank_id            uint16     `db:"rank_id"`
-	Update_date        []uint8    `db:"update_date"`
-	//Update_date *time.Time `db:"update_date"`
-	Update_string      string
-	Uncertain_prnt_ind *string `db:"uncertain_prnt_ind"`
-	N_usage            *string `db:"n_usage"`
-	Complete_name      string  `db:"complete_name"`
+	Tsn int64 `db:"tsn"   json:"tsn"`
+
+	Complete_name      *string    `db:"complete_name"  json:"completeName,omitempty"`
+	Completeness_rtng  *string    `db:"completeness_rtng"  json:",omitempty"`
+	Credibility_rtng   *string    `db:"credibility_rtng"  json:",omitempty"`
+	Currency_rating    *string    `db:"currency_rating"  json:"currencyRating,omitempty"`
+	Hybrid_author_id   int64      `db:"hybrid_author_id"  json:",omitempty"`
+	Initial_time_stamp *time.Time `db:"initial_time_stamp"  json:",omitempty"`
+	Kingdom_id         uint8      `db:"kingdom_id"  json:",omitempty"`
+	N_usage            *string    `db:"n_usage"  json:",omitempty"`
+	Name_usage         *string    `db:"name_usage"  json:",omitempty"`
+	Parent_tsn         *int64     `db:"parent_tsn"  json:",omitempty"`
+	Phylo_sort_seq     int32      `db:"phylo_sort_seq"  json:",omitempty"`
+	Rank_id            uint16     `db:"rank_id"  json:",omitempty"`
+	Taxon_author_id    int64      `db:"taxon_author_id"  json:",omitempty"`
+	Unaccept_reason    *string    `db:"unaccept_reason"  json:",omitempty"`
+	Uncertain_prnt_ind *string    `db:"uncertain_prnt_ind"  json:",omitempty"`
+	Unit_ind1          *string    `db:"unit_ind1" json:",omitempty"`
+	Unit_ind2          *string    `db:"unit_ind2"  json:",omitempty"`
+	Unit_ind3          *string    `db:"unit_ind3"  json:",omitempty"`
+	Unit_ind4          *string    `db:"unit_ind4"  json:",omitempty"`
+	Unit_name1         *string    `dbtsn,"unit_name1"  json:",omitempty"`
+	Unit_name2         *string    `db:"unit_name2"  json:",omitempty"`
+	Unit_name3         *string    `db:"unit_name3"  json:",omitempty"`
+	Unit_name4         *string    `db:"unit_name4"  json:",omitempty"`
+	Unnamed_taxon_ind  *string    `db:"unnamed_taxon_ind"  json:",omitempty"`
+	Update_date        *string    `db:"update_date"  json:"updateDate,omitempty"`
+	Update_date_tmp    []uint8    `db:"update_date"  json:"-"`
+	Update_string      *string    `json:"-"`
+}
+
+func indexAsKeyValue(table IsTable) {
+
+	db, err := connect()
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sql := "select * from " + table.Name()
+	fmt.Println(sql)
+	rows, err := db.Queryx(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	barType := reflect.ValueOf(table).Elem().Type()
+
+	for rows.Next() {
+		v := reflect.New(barType).Interface()
+
+		err = rows.StructScan(v)
+		keyFieldName := reflect.ValueOf(v).Elem().FieldByName(table.KeyFieldName())
+		fmt.Println(keyFieldName.Int())
+		fmt.Println("==")
+
+		var keyString string
+
+		switch keyFieldName.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			keyString = strconv.FormatInt(keyFieldName.Int(), 10)
+
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			keyString = strconv.FormatUint(keyFieldName.Uint(), 10)
+
+		case reflect.String:
+			keyString = string(keyFieldName.Bytes())
+		}
+
+		fmt.Println(keyString)
+
+		b, _ := json.Marshal(v)
+		fmt.Println(string(b))
+
+	}
 }
--- main.go
+++ main.go
@@ -1,61 +1,106 @@
 package main
 
 import (
-	//"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
 	"log"
-	"math/rand"
-	"strconv"
-	"time"
-	//	"os"
+	"net/http"
 )
 
-var baseDb *sqlx.DB
+var db *sqlx.DB
 
-const DB_FILE = "./itisSqlite122314/ITIS.sqlite"
+//const DB_FILE = "./data/itisSqlite/ITIS.sqlite"
+
+const DB_FILE = "file:///home/newtong/gocode/src/github.com/gnewton/goitis/data/itisSqlite/ITIS.sqlite?mode=ro"
+
+func tu(c web.C, w http.ResponseWriter, r *http.Request) {
+	tsn := c.URLParams["tsn"]
+	tu, err := getTaxonomicUnits2(tsn)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	if tu == nil {
+		w.WriteHeader(404)
+		w.Write([]byte(tsn + " not found"))
+		return
+	}
+
+	b, err := json.Marshal(tu)
+	w.Write(b)
+}
 
 func main() {
+	if true {
+		tu := new(TaxonomicUnit)
+		indexAsKeyValue(tu)
+		return
+	}
 	var err error
-	baseDb, err = connect()
+	db, err = connect()
 	if err != nil {
 		log.Fatal(err)
 	}
-	const maxTsn = 955136
-	for i := 1; i < maxTsn; i++ {
-		randomTsn := rand.Intn(maxTsn)
-		tu, err := getTaxonomicUnits(strconv.Itoa(randomTsn))
-		if err != nil {
-			fmt.Println(err)
-			log.Fatal(err)
-		}
-		fmt.Println(i)
-		if tu != nil {
-			tu.Update_string = string(tu.Update_date)
-			fmt.Printf("%+v\n", *tu)
-		} else {
-			fmt.Println("*")
-		}
-	}
-
+	goji.Get("/taxonomicunits/:tsn", tu)
+	goji.Serve()
 }
 
 func connect() (*sqlx.DB, error) {
 	return sqlx.Open("sqlite3", DB_FILE)
 }
 
-func getTaxonomicUnits(tsn string) (*TaxonomicUnit, error) {
+// func getTaxonomicUnits(tsn string) (*TaxonomicUnit, error) {
+
+// 	tu := TaxonomicUnit{}
+
+// 	//err := baseDb.Get(&tu, "select * from taxonomic_units where tsn="+tsn)
+// 	sql := "select * from taxonomic_units where tsn=" + tsn
+// 	fmt.Println(sql)
+// 	err := baseDb.Get(&tu, sql)
+
+// 	if err != nil && err.Error() == "sql: no rows in result set" {
+// 		return nil, nil
+// 	}
+
+// 	return &tu, err
+
+// }
+
+func getTaxonomicUnits2(tsn string) (*TaxonomicUnit, error) {
 
 	tu := TaxonomicUnit{}
+	db, err := connect()
+	defer db.Close()
 
-	//err := baseDb.Get(&tu, "select * from taxonomic_units where tsn="+tsn)
-	err := baseDb.Get(&tu, "select * from taxonomic_units where tsn="+tsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := db.Preparex("select * from taxonomic_units where tsn=?")
+	defer stmt.Close()
+	fmt.Println(*stmt)
+	row := stmt.QueryRowx(tsn)
+
+	err = row.StructScan(&tu)
 
 	if err != nil && err.Error() == "sql: no rows in result set" {
 		return nil, nil
 	}
+	if err != nil {
+		log.Println(err)
+	}
 
+	if tu.Update_date_tmp != nil {
+		//tu.Update_date = new(string)
+		tmp := string(tu.Update_date_tmp)
+		tu.Update_date = &tmp
+	}
 	return &tu, err
 
 }
